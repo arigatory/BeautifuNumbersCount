12ed49a8d1518c2be4f03926d68ff16b782fcb37
Ivan MSI <arigatory@gmail.com>
2022-08-25 23:04:22 +0300

    Add project files.

---
+++ BeautifuNumbersCount/Polynome.cs
@@ -0,0 +1,41 @@
+﻿namespace BeautifuNumbersCount;
+
+public class OGF
+{
+    private int[] Coefficients;
+
+    public OGF(int length)
+    {
+        Coefficients = Enumerable.Repeat(1, length).ToArray();
+    }
+
+    private OGF(int[] coefficients)
+    {
+        Coefficients = coefficients;
+    }
+
+    public static OGF operator *(OGF op1, OGF op2)
+    {
+        int[] coefficients
+            = new int[op1.Coefficients.Length + op2.Coefficients.Length - 1];
+
+        for (int i = 0; i < op1.Coefficients.Length; ++i)
+            for (int j = 0; j < op2.Coefficients.Length; ++j)
+                coefficients[i + j] += op1.Coefficients[i] * op2.Coefficients[j];
+        return new OGF(coefficients);
+    }
+
+    public long SumOfSquareCoefficients
+    {
+        get
+        {
+            long result = 0;
+            foreach (int coefficient in Coefficients)
+            {
+                result += coefficient * coefficient;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BeautifuNumbersCount/Program.cs
@@ -0,0 +1,12 @@
+﻿using BeautifuNumbersCount;
+const int numeraBase = 13;
+
+// Ordinary generating function (OGF)
+var ogf1 = new OGF(numeraBase);
+var ogf3 = ogf1 * ogf1 * ogf1;
+var ogf6 = ogf3 * ogf3;
+
+long result = ogf6.SumOfSquareCoefficients;
+result *= numeraBase;
+
+Console.WriteLine(result);
